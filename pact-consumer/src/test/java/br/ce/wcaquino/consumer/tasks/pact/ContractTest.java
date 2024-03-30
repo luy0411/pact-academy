411b3e39391af928ce6437f65417de0911f6915f
Luisin <luy0411@gmail.com>
2024-03-30 02:47:08 -0300

    provider server with pact dsl working fine

--- pact-consumer/src/test/java/br/ce/wcaquino/consumer/tasks/pact/ContractTest.java
+++ pact-consumer/src/test/java/br/ce/wcaquino/consumer/tasks/pact/ContractTest.java
@@ -1,5 +1,7 @@
 package br.ce.wcaquino.consumer.tasks.pact;
 
+import au.com.dius.pact.consumer.dsl.DslPart;
+import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit.PactProviderRule;
 import au.com.dius.pact.consumer.junit.PactVerification;
@@ -14,6 +16,7 @@ import org.junit.Test;
 import java.io.IOException;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.*;
 
 public class ContractTest {
@@ -23,13 +26,19 @@ public class ContractTest {
 
     @Pact(consumer = "BasicConsumer")
     public RequestResponsePact createPact (PactDslWithProvider builder){
+        DslPart body = new PactDslJsonBody()
+                .numberType("id", 1L)
+                .stringType("task")
+                .stringType("dueDate");
+
         return builder.given("There's a pact with id 1")
                       .uponReceiving("Retrieve task 1")
                             .path("/todo/1")
                             .method("GET")
                       .willRespondWith()
                             .status(200)
-                            .body("{\"id\":1,\"task\":\"pact\",\"dueDate\":\"2024-01-01\"}")
+                            .body(body)
+                            //.body("{\"id\":1,\"task\":\"pact\",\"dueDate\":\"2024-01-01\"}")
                      .toPact();
     }
 
@@ -41,8 +50,9 @@ public class ContractTest {
 
         Task task = consumer.getTask(1L);
 
-        assertThat(task.getId(), is(1l));
-        assertThat(task.getTask(), is("pact"));
+        assertThat(task.getId(), is(notNullValue()));
+        assertThat(task.getId(), is(1L));
+        assertThat(task.getTask(), is(notNullValue()));
     }
 
 
